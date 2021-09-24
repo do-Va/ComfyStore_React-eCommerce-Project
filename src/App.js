a40730cf11a36ddae9ae5c7ede9b17cc9e0401c2
doVa <doVaris@users.noreply.github.com>
2021-09-24 09:07:14 +0300

    Project Start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Navbar, Sidebar, Footer } from './components';
+
+function App() {
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+    </div>
+  );
+}
+
+export default App;
