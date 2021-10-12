1322941f6d50da4b9e2572c2c17c37933fdd999d
doVa <doVaris@users.noreply.github.com>
2021-10-12 12:04:00 +0300

    Setup List View

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { formatPrice } from '../utils/helpers'
-import { Link } from 'react-router-dom'
-const ListView = () => {
-  return <h4>list view</h4>
-}
+import React from 'react';
+import styled from 'styled-components';
+import { formatPrice } from '../utils/helpers';
+import { Link } from 'react-router-dom';
+
+const ListView = ({ products }) => {
+  return (
+    <Wrapper>
+      {products.map(product => {
+        const { id, image, name, price, description } = product;
+        return (
+          <article key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <h5 className="price">{formatPrice(price)}</h5>
+              <p>{description.substring(0, 150)}...</p>
+              <Link to={`/products/${id}`} className="btn">
+                Details
+              </Link>
+            </div>
+          </article>
+        );
+      })}
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -19,21 +39,26 @@ const Wrapper = styled.section`
     border-radius: var(--radius);
     margin-bottom: 1rem;
   }
+
   h4 {
     margin-bottom: 0.5rem;
   }
+
   .price {
     color: var(--clr-primary-6);
     margin-bottom: 0.75rem;
   }
+
   p {
     max-width: 45em;
     margin-bottom: 1rem;
   }
+
   .btn {
     font-size: 0.5rem;
     padding: 0.25rem 0.5rem;
   }
+
   @media (min-width: 992px) {
     article {
       display: grid;
@@ -42,6 +67,6 @@ const Wrapper = styled.section`
       align-items: center;
     }
   }
-`
+`;
 
-export default ListView
+export default ListView;
